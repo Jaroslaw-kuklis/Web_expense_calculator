3c2e1de68be2d1892d9b459d4563c926a5dbd355
unknown <jaroslaw.kuklis@gmail.com>
2022-08-08 22:02:28 +0200

    Adding files with initate flask framework

---
+++ website/template/auth.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/Logut')
+def logut():
+    return "<p>logut</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign Up</p>"
